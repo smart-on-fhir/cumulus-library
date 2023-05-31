72eab0d526e24e42be795dc41a159350df3ea773
matt garber <Matthew.Garber@childrens.harvard.edu>
2023-05-31 12:25:48 -0400

    Basic medicationrequest counts

--- cumulus_library/studies/core/medication_request.sql
+++ cumulus_library/studies/core/medication_request.sql
@@ -1,10 +1,10 @@
 -- ############################################################
 -- FHIR MedicationRequest : Med Orders *possibly* Administered
-CREATE TABLE core__medication_request AS
+CREATE TABLE core__medicationrequest AS
 SELECT
     mr.status,
     mr.intent,
-    mr.authoredon,
+    date(from_iso8601_timestamp(mr.authoredon)) AS authoredon,
     mr.category,
     med_coding.code AS code,
     med_coding.display AS display,
@@ -12,4 +12,29 @@ SELECT
     mr.id AS med_admin_id,
     mr.subject.reference AS subject_id
 FROM medicationrequest AS mr,
-    UNNEST(mr.medicationcodeableconcept.coding) AS t (med_coding); --noqa: AL05
+    unnest(mr.medicationcodeableconcept.coding) AS t (med_coding); --noqa: AL05
+
+
+CREATE OR REPLACE VIEW core__countmedicationrequest_month AS
+WITH powerset AS (
+    SELECT
+        count(DISTINCT cmr.subject_id) AS cnt_subject,
+        cmr.status,
+        cmr.intent,
+        date_trunc('month', cmr.authoredon) AS authored_month,
+        cmr.display,
+        cmr.system
+    FROM core__medicationrequest AS cmr
+    GROUP BY cube(cmr.status, cmr.intent, cmr.authoredon, cmr.display, cmr.system)
+)
+
+SELECT
+    cnt_subject AS cnt,
+    status,
+    intent,
+    authored_month,
+    display,
+    system
+FROM powerset
+WHERE cnt_subject >= 10
+ORDER BY cnt DESC;
