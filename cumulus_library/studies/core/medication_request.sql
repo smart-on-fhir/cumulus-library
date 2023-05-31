8a2c3ce62fb590e67f73eeb68e03620e8cb52d30
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-05-31 13:21:31 -0400

    now rnnorm only

--- cumulus_library/studies/core/medication_request.sql
+++ cumulus_library/studies/core/medication_request.sql
@@ -5,6 +5,8 @@ SELECT
     mr.status,
     mr.intent,
     date(from_iso8601_timestamp(mr.authoredon)) AS authoredon,
+    date_trunc('month', date(from_iso8601_timestamp(mr.authoredon)))
+    AS authoredon_month,
     mr.category,
     med_coding.code AS code,
     med_coding.display AS display,
@@ -12,29 +14,28 @@ SELECT
     mr.id AS med_admin_id,
     mr.subject.reference AS subject_id
 FROM medicationrequest AS mr,
-    unnest(mr.medicationcodeableconcept.coding) AS t (med_coding); --noqa: AL05
+    unnest(mr.medicationcodeableconcept.coding) AS t (med_coding) --noqa: AL05
+WHERE med_coding.system = 'http://www.nlm.nih.gov/research/umls/rxnorm';
 
 
-CREATE OR REPLACE VIEW core__countmedicationrequest_month AS
+CREATE OR REPLACE VIEW core__count_medicationrequest_month AS
 WITH powerset AS (
     SELECT
         count(DISTINCT cmr.subject_id) AS cnt_subject,
         cmr.status,
         cmr.intent,
-        date_trunc('month', cmr.authoredon) AS authored_month,
-        cmr.display,
-        cmr.system
+        cmr.authoredon_month,
+        cmr.display
     FROM core__medicationrequest AS cmr
-    GROUP BY cube(cmr.status, cmr.intent, cmr.authoredon, cmr.display, cmr.system)
+    GROUP BY cube(cmr.status, cmr.intent, cmr.authoredon_month, cmr.display)
 )
 
 SELECT
     cnt_subject AS cnt,
     status,
     intent,
-    authored_month,
-    display,
-    system
+    authoredon_month,
+    display
 FROM powerset
 WHERE cnt_subject >= 10
 ORDER BY cnt DESC;
