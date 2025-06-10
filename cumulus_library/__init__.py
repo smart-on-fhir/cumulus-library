905328ad633f60e70fc319714d764f764ce74284
Matt Garber <matthew.garber@childrens.harvard.edu>
2025-06-10 16:43:20 -0400

    dynamic git versioning

--- cumulus_library/__init__.py
+++ cumulus_library/__init__.py
@@ -17,4 +17,4 @@ from cumulus_library.template_sql.base_templates import get_template
 # is all code level. See template_sql for more information.
 
 __all__ = ["BaseTableBuilder", "CountsBuilder", "StudyConfig", "StudyManifest", "get_template"]
-__version__ = "4.5.0"
+__version__ = "1!0.0.0"
