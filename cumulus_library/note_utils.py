32a1e4f1ac0422dac61ac4e431d6b11c23d3c9e6
Michael Terry <michael.terry@childrens.harvard.edu>
2026-04-21 10:50:05 -0400

    Update to use cfs.FsPath

--- cumulus_library/note_utils.py
+++ cumulus_library/note_utils.py
@@ -1,13 +1,8 @@
 import binascii
-import dataclasses
-import json
-import os
 import pathlib
-import urllib.parse
 from collections.abc import Generator
 
 import cumulus_fhir_support as cfs
-import fsspec
 
 from cumulus_library import base_utils, databases
 
@@ -16,32 +11,22 @@ from cumulus_library import base_utils, databases
 #########################
 
 
-@dataclasses.dataclass(kw_only=True)
-class _FileInfo:
-    fs: fsspec.AbstractFileSystem
-    path: str
-    size: int
-
-
 class NoteSource:
     def __init__(
         self,
         note_dirs: list[str | pathlib.Path] | None = None,
         *,
         phi_dir: str | pathlib.Path | None = None,
-        s3_region: str | None = None,
     ):
-        self._files: list[_FileInfo] | None = None
+        self._files: dict[cfs.FsPath, int] | None = None
         self._total_size = 0
 
         self._note_dirs = note_dirs
-        self._s3_region = s3_region
 
         self._salt = None
         if phi_dir:
-            fs, phi_dir = self._get_fsspec_path(phi_dir)
-            with fs.open(os.path.join(phi_dir, "codebook.json")) as f:
-                codebook = json.load(f)
+            codebook_path = cfs.FsPath(phi_dir, "codebook.json")
+            codebook = codebook_path.read_json(default={})
             if id_salt := codebook.get("id_salt"):
                 self._salt = binascii.unhexlify(id_salt)
 
@@ -57,13 +42,13 @@ class NoteSource:
             task = progress.add_task(label, total=None)
             self._scan()
             progress.update(task, total=self._total_size)
-            for file in self._files:
+            for file, size in self._files.items():
                 offset = 0
-                for row in cfs.read_multiline_json_with_details(file.path, fsspec_fs=file.fs):
+                for row in cfs.read_multiline_json_with_details(file):
                     offset = row["byte_offset"]
                     progress.advance(task, offset)
                     yield row["json"]
-                progress.advance(task, file.size - offset)
+                progress.advance(task, size - offset)
 
     def _scan(self) -> None:
         """Lazily ensure that we've scanned files"""
@@ -71,23 +56,9 @@ class NoteSource:
             return  # already scanned
 
         self._files = self._flatten_note_dirs()
-        self._total_size = sum(f.size for f in self._files)
+        self._total_size = sum(self._files.values())
 
-    def _get_fsspec_path(self, path: str | pathlib.Path) -> tuple[fsspec.AbstractFileSystem, str]:
-        path = str(path)  # allow pathlib.Path objects but stringify them here
-        parsed = urllib.parse.urlparse(path)
-        protocol = parsed.scheme or "file"
-        path = path if protocol == "file" else os.path.abspath(path)
-
-        # Initialize a fsspec filesystem
-        options = {}
-        if protocol == "s3" and self._s3_region:
-            options["client_kwargs"] = {"region_name": self._s3_region}
-        fs = fsspec.filesystem(protocol, **options)
-
-        return fs, path
-
-    def _flatten_note_dirs(self) -> list[_FileInfo]:
+    def _flatten_note_dirs(self) -> dict[cfs.FsPath, int]:
         """Converts a list of folders into a list of found filenames.
 
         Flattening like this is useful for performance reasons, to only scan the dirs once.
@@ -96,17 +67,15 @@ class NoteSource:
         """
         note_types = {"DiagnosticReport", "DocumentReference"}
 
-        infos = []
+        infos = {}
         for one_dir in self._note_dirs or []:
-            fs, one_dir = self._get_fsspec_path(one_dir)
+            fspath = cfs.FsPath(one_dir)
 
             # Actually scan for matching files
-            paths = list(
-                cfs.list_multiline_json_in_dir(one_dir, note_types, fsspec_fs=fs, recursive=True)
-            )
-            sizes = fs.sizes(paths)
+            paths = list(cfs.list_multiline_json_in_dir(fspath, note_types, recursive=True))
+            sizes = fspath.fs.sizes(paths)
             for idx, path in enumerate(paths):
-                infos.append(_FileInfo(fs=fs, path=path, size=sizes[idx]))
+                infos[cfs.FsPath(path)] = sizes[idx]
 
         return infos
 
