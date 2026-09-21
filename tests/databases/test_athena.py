17708840c9226d8046c685bf9ac6eae201304c91
Matt Garber <matthew.garber@childrens.harvard.edu>
2026-09-21 14:04:38 -0400

    fixed mock issue

--- tests/databases/test_athena.py
+++ tests/databases/test_athena.py
@@ -471,22 +471,27 @@ def test_boto_fallback(mock_session):
 @mock.patch("botocore.session")
 @mock.patch("pyathena.cursor.Cursor")
 def test_refetch_creds(mock_cursor, mock_session):
-    mock_cursor.return_value.execute.side_effect = [pyathena.error.DatabaseError, "foo"]
+    mock_cursor.execute.side_effect = [pyathena.error.DatabaseError, "foo"]
     db = databases.AthenaDatabaseBackend(
         region="test",
         work_group="test",
         profile="test",
         schema_name="test",
     )
+
     db.connect()
-    res = db.cursor().execute("SELECT * FROM information_schema.tables")
+    cursor = db.cursor()
+    cursor.cursor = mock_cursor
+    res = cursor.execute("SELECT * FROM information_schema.tables")
     assert res == "foo"
     with pytest.raises(pyathena.error.DatabaseError):
-        mock_cursor.return_value.execute.side_effect = [
+        mock_cursor.execute.side_effect = [
             pyathena.error.DatabaseError,
             pyathena.error.DatabaseError,
         ]
-        res = db.cursor().execute("SELECT * FROM information_schema.tables")
+        cursor = db.cursor()
+        cursor.cursor = mock_cursor
+        res = cursor.execute("SELECT * FROM information_schema.tables")
 
 
 @mock.patch.dict(
